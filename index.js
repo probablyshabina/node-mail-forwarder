6d859125319b91480707259669a218d617a8fb45
Shabina <shabinafarveen@gmail.com>
2023-04-15 16:05:16 +0530

    Retirves the mail from DB and forwards the content but only works with localhost

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
-require('dotenv').config();
+import { } from 'dotenv/config'
+import fs from 'fs'
+import sendMail from './sendMail.js'
 
-const nodeMailer = require('nodemailer');
+console.log("Staring");
 
-const emailContentHTML = `
-    <h1>Invitation to Complete Tasks for SLIIT FOSS Community Membership Application</h1>
-    <p>This is a test email but I want to say I love you</p>`;
-
-async function main(){
-    console.log("Staring");
-    let transporter = nodeMailer.createTransport({
-        service : "gmail",
-        auth: {
-            type : "OAuth2",
-            user: process.env.MAIL_USERNAME,
-            pass : process.env.MAIL_PASSWORD,
-            clientId : process.env.OAUTH_CLIENTID,
-            clientSecret : process.env.OAUTH_CLIENT_SECRET,
-            refreshToken : process.env.OAUTH_REFRESH_TOKEN,
-        }
+async function sendMailsToRecipients() {
+    await fetch('http://localhost:8000/api/v1/recipients/', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
     })
-    
-    let mailOptions = {
-        from : "shabinafarveen@gmail.com",
-        to : ["shabeenaliyaudeen@gmail.com", "shabeenafarveen@gmail.com"],
-        subject : "Invitation to Complete Tasks for SLIIT FOSS Community Membership Application",
-        html : emailContentHTML,
-    }
-    
-    let info = await transporter.sendMail(mailOptions, function(err, data) {
-        if (err) {
-          console.log("Error " + err);
-        } else {
-            if(data.accepted.length > 0)
-                console.log("Email sent successfully to " + data.accepted.join() + " !");
-            if(data.rejected.length > 0)
-                console.log("Email failed to send to " + data.rejected.join() + " !");
-        }
-      });
-    //console.log("Message sent: %s", info.messageId);
-}
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Network response was not ok');
+            }
+        })
+        .then(data => {
+            data.recipients.map(recipient => {
+                var sendTo = recipient.email
+                let htmlContent = fs.readFileSync("./mail.html", "utf8");
+                htmlContent = htmlContent.replace("{{name}}", `${recipient.name}`);
+                htmlContent = htmlContent.replace("{{link}}", `http://localhost:8000/api/v1/recipients/click/${recipient._id}`);
+                sendMail(recipient.email, htmlContent, recipient._id).catch(console.error);
+            })
+        })
+        .catch(error => {
+            console.error(error);
+        });
 
-main().catch(console.error);
+}
+sendMailsToRecipients();
